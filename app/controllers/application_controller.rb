8ed74d4a210cf857a49236df24dfd170a4bf86eb
takahiro403 <f74.sonoda.takahiro@gmail.com>
2020-11-08 08:28:33 +0900

    basic認証を無効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  # ポートフォリオ用のためbasic認証は無効
+  # before_action :basic_auth
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -20,9 +21,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic][:BASIC_AUTH_USER] && password == Rails.application.credentials[:basic][:BASIC_AUTH_PASSWORD]
-    end
-  end
+  #def basic_auth
+    #authenticate_or_request_with_http_basic do |username, password|
+      #username == Rails.application.credentials[:basic][:BASIC_AUTH_USER] && password == Rails.application.credentials[:basic][:BASIC_AUTH_PASSWORD]
+    #end
+  #end
 end
